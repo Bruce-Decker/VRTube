aba3d0afe7fabd8c0315d1520f52fd0060a41bcc
Matthew Kwong <mtkwong91@gmail.com>
2018-04-09 14:23:15 -0700

    restructured as standard node app; replace jade with ejs

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const PORT = 5000;
 
 var app = express();
 
+var server = app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+var index = require('./routes/index');
+var vr = require('./routes/vr');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/vr', vr);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
