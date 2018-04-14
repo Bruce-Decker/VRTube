9249cf1c39df34e955d18a0f7faf92dbbcf17817
akashgupta2903 <akash.gupta@gmail.com>
2018-04-13 17:27:15 -0700

    adding signup API

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var app = express();
 
 var index = require('./routes/index');
 var vr = require('./routes/vr');
+var users = require('./routes/users');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/vr', vr);
+app.use('/users' , users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/users.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('./mysql');
+var bcrypt = require('bcrypt');
+var path = require('path');
+var mkdir = require('mkdirp');
+const saltRnd = 3;
+
+router.get('/login', function(req, res, next) {
+  console.log("inside login");
+});
+
+
+router.post('/signup', function(req, res, next) {
+  var firstName = req.body.firstname;
+  var lastName = req.body.lastname;
+  var userName = req.body.username;
+  var password = req.body.password;
+  var contact = req.body.contactinfo;
+  let res_result =  {
+                      message:''
+                    };
+
+  bcrypt.hash(password, saltRnd, function(err, hashpassword) {
+    if(!err) {
+      var userHome = userName;
+      var createUserQuery = "insert into user (firstname, lastname, email, password, homedir,contact) values ('"+
+      firstName + "','" + lastName + "','" + userName + "','" + hashpassword + "','" + userHome +"','"+contact+"')";
+
+      mysql.executeSQLQuery(createUserQuery, function(err, result){
+        if(err){
+          res_result.message = "Registration failed !!!"
+          res.status(400).json(res_result);
+        }else{
+          var success_msg = "User "+ firstName+" added successfully !!!"
+          mkdir(path.join(__dirname , './../uploads' , userHome) , function(status) {
+            if(status) {
+              res_result.message = "Registration successfully !!!"
+              res.status(200).json(res_result);
+            }else {
+              console.log("[Unable to create directory] for user:" + userName);
+            }
+          });
+        }
+      });
+    }
+  });
+});
+
+module.exports = router;
